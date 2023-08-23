4b9d01ed0f0d283ed5632a19742c66e5fff63ab7
aurelija-93 <aurelija.bellerby@gmail.com>
2023-08-23 14:20:40 +0100

    created Hero class

--- src/main/java/heroes/Hero.java
+++ src/main/java/heroes/Hero.java
@@ -1,4 +1,50 @@
 package heroes;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Hero {
+    private long health;
+    private int damage;
+    private int xp;
+    private int level;
+    private long gold;
+    private ArrayList<String> inventory;
+
+    public Hero(long health, int damage, int xp, int level, long gold, ArrayList<String> inventory) {
+        this.health = health;
+        this.damage = damage;
+        this.xp = xp;
+        this.level = level;
+        this.gold = gold;
+        this.inventory = inventory;
+    }
+
+    public long getHealth() {
+        return health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getXp() {
+        return xp;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public long getGold() {
+        return gold;
+    }
+
+    public ArrayList<String> getInventory() {
+        return inventory;
+    }
+
+    public void addToInventory(String item) {
+        inventory.add(item);
+    }
 }
--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -1,2 +1,54 @@
+import heroes.Hero;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class HeroTest {
+    private Hero hero;
+    private ArrayList<String> inventory;
+
+    @Before
+    public void before() {
+        inventory = new ArrayList<>();
+        hero = new Hero(100, 20, 50, 1, 70, inventory);
+    }
+
+    @Test
+    public void hasHealth() {
+        assertEquals(100, hero.getHealth());
+    }
+
+    @Test
+    public void hasDamage() {
+        assertEquals(20, hero.getDamage());
+    }
+
+    @Test
+    public void hasXp() {
+        assertEquals(50, hero.getXp());
+    }
+
+    @Test
+    public void hasLevel() {
+        assertEquals(1, hero.getLevel());
+    }
+
+    @Test
+    public void hasGold() {
+        assertEquals(70, hero.getGold());
+    }
+
+    @Test
+    public void inventoryStartsEmpty() {
+        assertEquals(0, hero.getInventory().size());
+    }
+
+    @Test
+    public void canAddToInventory() {
+        hero.addToInventory("Healing potion");
+        assertEquals(1, hero.getInventory().size());
+    }
 }
